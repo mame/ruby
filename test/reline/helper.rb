316b197c2d3c8bd648d95cd74cb94398019af71a
Yusuke Endoh <mame@ruby-lang.org>
2021-12-24 12:44:49 +0900

    tmp

--- test/reline/helper.rb
+++ test/reline/helper.rb
@@ -41,6 +41,7 @@ class Reline::TestCase < Test::Unit::TestCase
       if Reline::Unicode::EscapedChars.include?(c.ord)
         c
       else
+        p [:encoding, @line_editor.instance_variable_get(:@encoding)]
         c.encode(@line_editor.instance_variable_get(:@encoding), Encoding::UTF_8, **options)
       end
     }.join
