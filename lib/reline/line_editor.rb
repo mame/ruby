1087721506ec1252fdefb3c21b63de97d5e97933
Yusuke Endoh <mame@ruby-lang.org>
2021-12-24 14:12:07 +0900

    tmp4

--- lib/reline/line_editor.rb
+++ lib/reline/line_editor.rb
@@ -1485,7 +1485,7 @@ class Reline::LineEditor
   end
 
   private def process_key(key, method_symbol)
-    p [:bar, key, method_symbol]
+    $stderr.puts [:bar, key, method_symbol].inspect
     if method_symbol and respond_to?(method_symbol, true)
       method_obj = method(method_symbol)
     else
